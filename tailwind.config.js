9b24a4c3d46fdb74a423d18bfbecdb843ee12b8d
robinsector <robbin@sector.co.id>
2024-07-28 22:27:23 +0700

    Adjust Customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,96 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary-color": "#33568B",
+        "primary-light-color": "#E3EDFB",
+        "primary-font-color": "#15345A",
+        "secondary-gold-color": "#D1B052",
+        "secondary-green-color": "#389181",
+        "font-description-color": "#888888",
+      },
+      fontSize: {
+        "heading-1": [
+          "46px",
+          {
+            lineHeight: "55px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+        "heading-3": [
+          "42px",
+          {
+            lineHeight: "50px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+        "heading-4": [
+          "40px",
+          {
+            lineHeight: "48px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+        "heading-6": [
+          "32px",
+          {
+            lineHeight: "38px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+        "LG/LG-Normal": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+        "LG/LG-Strong": [
+          "16px",
+          {
+            lineHeight: "24px",
+            fontWeight: "600",
+            // letterSpacing: "1px",
+          },
+        ],
+        "XL/XL-Normal": [
+          "20px",
+          {
+            lineHeight: "28px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+        "XL/XL-Strong": [
+          "20px",
+          {
+            lineHeight: "28px",
+            fontWeight: "600",
+            // letterSpacing: "1px",
+          },
+        ],
+        "Base/Base-Strong": [
+          "14px",
+          {
+            lineHeight: "22px",
+            fontWeight: "600",
+            // letterSpacing: "1px",
+          },
+        ],
+        "Base/Base-Normal": [
+          "14px",
+          {
+            lineHeight: "22px",
+            fontWeight: "400",
+            // letterSpacing: "1px",
+          },
+        ],
+      },
     },
   },
   plugins: [],
