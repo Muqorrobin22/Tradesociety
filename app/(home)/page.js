7d245a315335cc1a43e4b346c13b4614a58cb886
robinsector <robbin@sector.co.id>
2024-07-29 02:20:53 +0700

    Hero Section CTA Image addons fix

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -23,7 +23,7 @@ export default function Home() {
   {/*  Hero Section CTA - Start */}
 
     <section className={clsx("relative mt-[180px] h-[306px] flex items-center px-[100px] ")}>
-      <Image src={"/images/components/addons/Hero_CTA_addons.png"} alt={""} height={306} width={1198} className={clsx("absolute z-10 left-[50%] translate-x-[-50%]")} />
+      <Image src={"/images/components/addons/hero_cta_revision_addons.png"} alt={""} height={306} width={1198} className={clsx("absolute z-10 left-[50%] translate-x-[-50%]")} />
       <div className={clsx("z-20 flex justify-between items-center")}>
         <h1 className={clsx("text-primary-font-color text-heading-4 basis-2/3 ")}>
           Belajar trading <span className={clsx("text-secondary-gold-color")}>100% gratis!</span> <br/> Ikuti kelas
