0d3d25369f9d2658b374744c9cf33458e2293040
robinsector <robbin@sector.co.id>
2024-08-04 23:12:32 +0700

    Responsive - About us: Responsive done

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -6,9 +6,9 @@ export default function AboutUsPage() {
     return <main className={clsx("mt-10")}>
         {/* Hero Section */}
         <section
-            className={clsx("relative bg-linear-gradient-hero h-[524px] px-[72px] rounded-[32px] flex items-center ")}>
+            className={clsx("relative bg-linear-gradient-hero desktop:h-[524px] px-[72px] rounded-[32px] flex desktop:items-center desktop:justify-normal mobile:flex-col desktop:flex-row mobile:justify-center mobile:items-center  mobile:mt-[120px] desktop:mt-0 mobile:p-4  ")}>
 
-            <section className={clsx("text-left  w-[500px] ")}>
+            <section className={clsx("text-left  desktop:w-[500px] ")}>
                 <h1 className={clsx("text-white text-heading-1 mx-auto ")}>Platform untuk para trader</h1>
                 <p className={clsx("text-XL/XL-Normal text-[#F6F6F6] text-left mx-auto  mt-2 ", fonts.roboto)}>Bukan
                     Kami memberikan peluang untuk trader pemula hingga profesional untuk bisa berhasil di industri
@@ -26,10 +26,10 @@ export default function AboutUsPage() {
 
         {/*  Why we here - Start  */}
 
-        <section className={clsx("mt-[80px] py-[40px] relative")}>
+        <section className={clsx("mt-[80px] py-[40px] relative mobile:px-4 desktop:px-0")}>
             <section className={clsx("flex flex-col justify-center items-center relative z-20")}>
                 <p className={clsx(" text-Base/Base-Strong text-[#1761B1]  ", fonts.roboto)}>Why we here</p>
-                <h3 className={clsx("text-heading-3 text-[#15345A] my-4 ")}>Bersama menuju kesuksesan di industri trading</h3>
+                <h3 className={clsx("text-heading-3 text-[#15345A] my-4 desktop:text-left mobile:text-center ")}>Bersama menuju kesuksesan di industri trading</h3>
                 <p className={clsx("text-XL/XL-Normal text-font-description-color max-w-[800px] text-center", fonts.roboto)}>Ada banyak cara untuk sukses dalam berkarir di industri trading yang belum banyak diketahui. Di sini, kami memberikan edukasi secara menyeluruh untuk sukses, tidak hanya sebagai trader, tetapi juga dalam peran profesional lainnya dalam industri trading.
                     sini!</p>
 
@@ -42,11 +42,11 @@ export default function AboutUsPage() {
 
         {/*  Why we here - Start  */}
 
-        <section className={clsx("mt-[80px] py-[40px] px-[140px] ")}>
-            <div className={clsx("p-8 rounded-[32px] bg-[#F4F8FD]  flex items-center ")}>
-                <Image src={"/images/components/illustration/about_us_vision.svg"} alt={"vision"} width={250}
-                       height={250}/>
-                <div className={clsx("ml-[64px]")}>
+        <section className={clsx("mt-[80px] desktop:py-[40px] desktop:px-[140px] mobile:px-4 ")}>
+            <div className={clsx("p-8 rounded-[32px] bg-[#F4F8FD]  flex desktop:items-center desktop:justify-normal desktop:flex-row mobile:flex-col ")}>
+                <Image src={"/images/components/illustration/about_us_vision_revision.svg"} alt={"vision"} width={180}
+                       height={180} className={clsx("desktop:order-1 mobile:order-2 mobile:mx-auto desktop:m-0 mobile:mt-8")} />
+                <div className={clsx("desktop:ml-[64px] desktop:order-2 ")}>
                     <p className={clsx(" text-Base/Base-Strong text-[#1761B1]  ", fonts.roboto)}>Vision</p>
                     <h3 className={clsx("text-heading-3 text-[#15345A] my-2 ")}>Centralized Platform</h3>
                     <p className={clsx("text-XL/XL-Normal text-font-description-color  text-left", fonts.roboto)}>Ada
@@ -55,16 +55,16 @@ export default function AboutUsPage() {
                 </div>
             </div>
 
-            <div className={clsx("p-8 rounded-[32px] bg-[#F4F8FD]  flex items-center mt-8 ")}>
+            <div className={clsx("p-8 rounded-[32px] bg-[#F4F8FD]  flex desktop:items-center desktop:justify-normal desktop:flex-row mobile:flex-col mt-8 ")}>
 
-                <div className={clsx("mr-[64px]")}>
+                <div className={clsx("desktop:mr-[64px]")}>
                     <p className={clsx(" text-Base/Base-Strong text-[#1761B1]  ", fonts.roboto)}>Mission</p>
                     <h3 className={clsx("text-heading-3 text-[#15345A] my-2 ")}>Learning center and growth opportunities</h3>
                     <p className={clsx("text-XL/XL-Normal text-font-description-color  text-left", fonts.roboto)}>
                         Menyediakan sumber belajar terbaik dari para ahli trading di berbagai instrumen finansial dan memberikan peluang bagi para trader untuk memperbesar dampak mereka di industri trading.</p>
                 </div>
                 <Image src={"/images/components/illustration/about_us_mission.svg"} alt={"vision"} width={250}
-                       height={250}/>
+                       height={250} className={clsx("desktop:m-0 mobile:mx-auto mobile:mt-8")} />
             </div>
         </section>
 
@@ -72,18 +72,18 @@ export default function AboutUsPage() {
 
         {/*  Jadi bagian dari tim- Start  */}
 
-            <section className={clsx("mt-[80px] py-[80px] px-[92px] relative ")}>
+            <section className={clsx("mt-[80px] desktop:py-[80px] mobile:py-[40px] desktop:px-[92px] mobile:px-8 relative ")}>
                 <div className={"relative z-20"}>
                     <div className={clsx("text-center")}>
-                        <h3 className={clsx("text-heading-3 text-[#15345A] mb-4 ")}> jadi bagian dari tim</h3>
+                        <h3 className={clsx("text-heading-3 text-[#15345A] mb-4 ")}> gabung dengan trade society!</h3>
                         <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center", fonts.roboto)}>
-                           Bersama kita kembangkan Trade Society.</p>
+                            Temukan program yang sesuai dengan kemampuan dan skillmu!</p>
                     </div>
-                    <div className={clsx("mt-8 rounded-[32px] border-[1px] border-[#E3EDFB] grid grid-cols-2 ")}>
-                        <div className={clsx("p-8 items-center justify-center ")}>
+                    <div className={clsx("mt-8 rounded-[32px] border-[1px] border-[#E3EDFB] grid desktop:grid-cols-2 ")}>
+                        <div className={clsx("p-8 items-center justify-center  border-[#E3EDFB] mobile:border-b-[1px] desktop:border-b-0 ")}>
                             <div className={"text-center"}>
                                 <h3 className={clsx("text-heading-6 text-[#15345A] mb-4 ")}> Mentor</h3>
-                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center w-[350px] mx-auto ", fonts.roboto)}>
+                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center desktop:w-[350px] mx-auto ", fonts.roboto)}>
                                     Bertanggung jawab sebagai pengajar  dari kelas-kelas yang ada untuk setiap  komunitas Trade Society.</p>
                             </div>
                         </div>
@@ -91,7 +91,7 @@ export default function AboutUsPage() {
                         <div className={clsx("p-8 items-center justify-center border-[#E3EDFB] border-l-[1px] ")}>
                             <div className={"text-center"}>
                                 <h3 className={clsx("text-heading-6 text-[#15345A] mb-4 ")}> Analyst</h3>
-                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center w-[350px] mx-auto ", fonts.roboto)}>
+                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center desktop:w-[350px] mx-auto ", fonts.roboto)}>
                                     Bertanggung jawab terhadap  signal trading dan market outlook  Trade society.</p>
                             </div>
                         </div>
@@ -99,7 +99,7 @@ export default function AboutUsPage() {
                         <div className={clsx("p-8 items-center justify-center border-t-[1px] border-[#E3EDFB]  ")}>
                             <div className={"text-center"}>
                                 <h3 className={clsx("text-heading-6 text-[#15345A] mb-4 ")}> leaders</h3>
-                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center w-[350px] mx-auto ", fonts.roboto)}>
+                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center desktop:w-[350px] mx-auto ", fonts.roboto)}>
                                     Bertanggung jawab sebagai ketua  di seluruh komunitas Trade Society</p>
                             </div>
                         </div>
@@ -107,7 +107,7 @@ export default function AboutUsPage() {
                         <div className={clsx("p-8 items-center justify-center border-t-[1px] border-[#E3EDFB] border-l-[1px] ")}>
                             <div className={"text-center"}>
                                 <h3 className={clsx("text-heading-6 text-[#15345A] mb-4 ")}> consultant</h3>
-                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center w-[350px] mx-auto ", fonts.roboto)}>
+                                <p className={clsx("text-XL/XL-Normal text-font-description-color  text-center desktop:w-[350px] mx-auto ", fonts.roboto)}>
                                     Bertanggung jawab terhadap proses  onboarding calon member dan
                                     bimbingan member di komunitas.</p>
                             </div>
