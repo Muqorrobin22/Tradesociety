013cf6e436c88db17d77e7bf615431d583c8a7c2
robinsector <robbin@sector.co.id>
2024-08-10 15:20:37 +0700

    Components: FAQ Cards functionality is done

--- app/_ui/Micro-Component/FAQ/FAQCard.js
+++ app/_ui/Micro-Component/FAQ/FAQCard.js
@@ -1,9 +1,11 @@
 import clsx from "clsx";
 import Image from "next/image";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {fonts} from "@/app/_lib/utils/fonts/fonts";
+import {setShowFaq} from "@/app/_lib/store/features/faqslices/faqSlices";
 
 export default function FAQCard({onClick, activeIndex, theAnswer, theQuestion, height = "h-[200px]", idleHeight = "h-[92px]"}) {
+    const dispatch = useDispatch()
 
     const showFaq = useSelector((state) => state.faq.showFaq)
 
@@ -15,7 +17,9 @@ export default function FAQCard({onClick, activeIndex, theAnswer, theQuestion, h
             <div className={clsx("flex items-center justify-between")}>
                 <h5 className={clsx("text-XL/XL-Strong text-[#15345A] ", fonts.roboto)}>{theQuestion}</h5>
                 <Image src={"/images/components/Icon/plus-circle.svg"} alt={"Plus icon"} width={24} height={24}
-                       className={"hover:cursor-pointer ml-8"} onClick={onClick}/>
+                       className={clsx("hover:cursor-pointer ml-8", showFaq === activeIndex ? "hidden" : "visible")} onClick={onClick}/>
+                <Image src={"/images/components/icon_new/minus_circle_icon.svg"} alt={"Minus icon"} width={24} height={24}
+                       className={clsx("hover:cursor-pointer ml-8", showFaq === activeIndex ? "visible" : "hidden")} onClick={() => dispatch(setShowFaq(true))}/>
             </div>
             <p className={clsx("text-LG/LG-Normal text-font-description-color mt-6", fonts.roboto, showFaq === activeIndex ? "visible" : "hidden")}> {theAnswer} </p>
         </div>
