090e9a8f2cfc354aa3aac4826a4e6c6e26b1f426
robinsector <robbin@sector.co.id>
2024-08-11 21:57:46 +0700

    Referral: Add new Referral

--- app/_lib/const/REFERRAL.js
+++ app/_lib/const/REFERRAL.js
@@ -3,9 +3,24 @@ export function GetReferralLinkTelegram(referral) {
     switch (referral) {
         case "IkyyFX":
             return "https://t.me/ikyyFXTrade"
-        break;
         case "imTradeFX":
             return "https://t.me/im_TraderFX";
+        case "DatsFx":
+            return "https://t.me/DatsFxgold";
+        case "DenFX":
+            return "https://t.me/DenFXTrade";
+        case "RipFxs":
+            return "https://t.me/RipFXs";
+        case "rasyatrade":
+            return "https://t.me/rasyatradefxgold";
+        case "FXcmdr":
+            return "https://t.me/FXCommanderr";
+        case "JarBoyFX":
+            return "https://t.me/JarBoyFX";
+        case "tradeefriendly":
+            return "https://t.me/tradeefriendly";
+        case "Saylor90":
+            return "https://t.me/Saylor90 ";
         default:
             return "https://t.me/m/am6DU9LKNDI1";
     }
