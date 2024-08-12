71d2eb2c9c29485a4e43f3e50510d9c97ff2f63c
robinsector <robbin@sector.co.id>
2024-08-13 02:52:23 +0700

    Aceternity UI: Create Card hover effect for Contact-us page Contact Cards

---
+++ components/ui/card-hover-effect-contact.js
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+
+export const HoverEffectContact = ({ items, className }) => {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    return (
+        <div
+            className={cn(
+                "",
+                className
+            )}
+        >
+            {items.map((item, idx) => (
+                <div
+                    key={item.id}
+                    className="relative group block p-1 "
+                    onMouseEnter={() => setHoveredIndex(idx)}
+                    onMouseLeave={() => setHoveredIndex(null)}
+                >
+                    <AnimatePresence>
+                        {hoveredIndex === idx && (
+                            <motion.span
+                                className="absolute inset-0 h-full w-full bg-primary-light-color block rounded-[32px] "
+                                layoutId="hoverBackground"
+                                initial={{ opacity: 0 }}
+                                animate={{
+                                    opacity: 1,
+                                    transition: { duration: 0.15 },
+                                }}
+                                exit={{
+                                    opacity: 0,
+                                    transition: { duration: 0.15, delay: 0.2 },
+                                }}
+                            />
+                        )}
+                    </AnimatePresence>
+                    <div className={"bg-white rounded-[32px]  relative z-20"}>
+                        {item.elements}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
