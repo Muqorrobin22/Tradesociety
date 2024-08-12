6cbb84ae4a1d2497ec33727c5c1e6413e63cad5d
robinsector <robbin@sector.co.id>
2024-08-13 02:53:06 +0700

    Components: Adjust translate-y form 5px to 3px

--- app/_ui/Components/Cards/ContactusCard.js
+++ app/_ui/Components/Cards/ContactusCard.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function ContactUsCard({Sosmed, imageSrc, rounded, href}) {
     return (
         <a href={href} target="_blank"
-            className={clsx("w-auto  border-[1px] border-[#E3EDFB]  relative block hover:translate-y-[-5px] transition-all duration-300 p-8 ", rounded)}>
+            className={clsx("w-auto  border-[1px] border-[#E3EDFB]  relative block hover:translate-y-[-3px] transition-all duration-300 p-8 ", rounded)}>
             <div className={clsx("relative my-auto ")}>
                 <Image src={imageSrc} alt={Sosmed} width={125} height={141} className={clsx('mx-auto my-auto')}  />
             </div>
