f340bf63662f3c31229b9baf1211cba398eb0aa2
robinsector <robbin@sector.co.id>
2024-08-14 23:08:27 +0700

    coba lagi deh

--- app/(tradesociety)/our-program/programs/newbie/page.js
+++ app/(tradesociety)/our-program/programs/newbie/page.js
@@ -42,11 +42,11 @@ export default function NewbiePage() {
                 setScrollPositionCard(position);
                 setScrollPositionDescriptionBar(position)
 
-                if(position > 3320 || position < 367) {
-                    setIsReactBottom("")
-                } else {
-                    setIsReactBottom("active")
-                }
+                // if(position > 3320 || position < 367) {
+                //     setIsReactBottom("")
+                // } else {
+                //     setIsReactBottom("active")
+                // }
 
                 if(position > 605) {
                     setScrollNavbarLink(position)
