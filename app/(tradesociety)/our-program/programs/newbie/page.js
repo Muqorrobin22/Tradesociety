fdbc790f87ebee47c2b7c9faf394d306d33def2f
robinsector <robbin@sector.co.id>
2024-08-14 22:42:30 +0700

    Newbie page change state from boolean to string

--- app/(tradesociety)/our-program/programs/newbie/page.js
+++ app/(tradesociety)/our-program/programs/newbie/page.js
@@ -33,7 +33,7 @@ export default function NewbiePage() {
     const [scrollPositionDescriptionBar, setScrollPositionDescriptionBar] = useState(0);
     const [scrollActiveState, setScrollActiveState] = useState("");
     const [scrollNavbarLink, setScrollNavbarLink] = useState(0);
-    const [isReactBottom, setIsReactBottom] = useState(true);
+    const [isReactBottom, setIsReactBottom] = useState("inactive");
 
     useEffect(() => {
         if(typeof  window !== "undefined") {
@@ -53,14 +53,19 @@ export default function NewbiePage() {
                 }
                 if (position >= 1360) {
                     setScrollActiveState("jadwal_kelas");
+                    setIsReactBottom("inactive")
                 } else if (position >= 1082) {
                     setScrollActiveState("benefits");
+                    setIsReactBottom("active")
                 } else if (position >= 705) {
                     setScrollActiveState("materi");
+                    setIsReactBottom("active")
                 } else if (position >= 510) {
                     setScrollActiveState("Description");
+                    setIsReactBottom("active")
                 } else {
                     setScrollActiveState("");
+                    setIsReactBottom("inactive")
                 }
 
 
@@ -445,7 +450,7 @@ export default function NewbiePage() {
 
                 <section className={"  desktop:basis-1/3 mobile:basis-0 desktop:ml-[84px]  z-30 "}>
                     <div
-                        className={clsx("rounded-[32px] border-[1px] border-[#E3EDFB] bg-white p-8 desktop:sticky mobile:fixed  desktop:top-[94px] drop-shadow-custom-cards ", isReactBottom === true ? "mobile:hidden desktop:visible desktop:block" : " mobile:bottom-0 mobile:left-0 mobile:right-0")}>
+                        className={clsx("rounded-[32px] border-[1px] border-[#E3EDFB] bg-white p-8 desktop:sticky mobile:fixed  desktop:top-[94px] drop-shadow-custom-cards ", isReactBottom === "inactive" ? "mobile:hidden desktop:visible desktop:block" : " mobile:bottom-0 mobile:left-0 mobile:right-0")}>
 
 
                         <div
